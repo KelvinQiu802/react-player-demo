8be4c6e000670a671d053984f10fe181ee3ad786
KelvinQiu <kelvinqiu802@outlook.com>
2024-01-16 21:31:20 +0800

    feat: demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ReactPlayer from 'react-player/youtube';
+import { FullScreen, useFullScreenHandle } from 'react-full-screen';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const handle = useFullScreenHandle();
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handle.enter} style={{ position: 'absolute' }}>
+        Enter
+      </button>
+      <FullScreen handle={handle}>
+        <div style={{ overflow: 'hidden' }}>
+          <button
+            onClick={handle.exit}
+            style={{ position: 'absolute', top: 500, left: 500 }}
+          >
+            Exit
+          </button>
+          <ReactPlayer
+            url={'https://www.youtube.com/watch?v=u9vK5utTcxE=1'}
+            playing
+            muted
+            controls
+            width={'100vw'}
+            height={'100vh'}
+            style={{ pointerEvents: 'none' }}
+          />
+        </div>
+      </FullScreen>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
